130c57ec6427a70ef79f263fcf407a5dabc74f58
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-07-12 19:43:39 +0530

    evening mood

---
+++ Exercises/Ex1-SimpleCalculator1.py
@@ -0,0 +1,8 @@
+X=40
+Y=20
+
+print("Addition",X+Y)
+print("Subtraction",X-Y)
+print("Multiplication",X*Y)
+print("Division",X/Y)
+print("Modulus",X%Y)
---
+++ Exercises/Ex1-SimpleCalculator2.py
@@ -0,0 +1,10 @@
+X = 50
+Y = 4
+
+print("The value of", X, "+", Y, "is: ", X + Y)
+print("The value of", X, "-", Y, "is: ", X - Y)
+print("The value of", X, "*", Y, "is: ", X * Y)
+print("The value of", X, "/", Y, "is: ", X / Y)
+print("The value of", X, "//", Y, "is: ", X // Y)
+print("The value of", X, "%", Y, "is: ", X % Y)
+print("The value of", X, "**", Y, "is: ", X ** Y)
