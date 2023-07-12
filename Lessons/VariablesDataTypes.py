ff6da4fc79d6b44a0dc6b9abea084785a375a2c0
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-07-12 09:48:37 +0530

    morning

---
+++ Lessons/VariablesDataTypes.py
@@ -0,0 +1,16 @@
+a=15
+print(a)
+b="Shubham"
+print(b)
+
+#Sequenced data types: Lists, Tuples
+
+list1= [8, 2.3, [-4,5],["Shubham"]]
+print(list1)  #Mutable
+
+tuple1=(("X","Y","Z"),("Lion", "Tiger"))
+print(tuple1)  #Immutable
+
+# Mapped Data Type = Dictionary (Key:Value Pair Of Data)
+dict1={"Name":"SHUBHAM","Age":22,"canDrive":True}
+print(dict1)
