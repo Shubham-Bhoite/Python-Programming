b7432439e17ee4fd08083cfad008104ea9a6a86b
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-07-14 17:47:37 +0530

    evening

---
+++ Lessons/String-Slicing.py
@@ -0,0 +1,18 @@
+# Length od String
+fruit = "Banana"
+len1 = len(fruit)
+print("Banana is a", len1,"letter")
+
+
+# String-Slicing
+pie = "ApplePie"
+print(pie[:5])      #Slicing from Start
+print(pie[5:])      #Slicing till End
+print(pie[2:6])     #Slicing in between
+print(pie[-8:])     #Slicing using negative index
+
+
+# Loop through String
+alphabets = "XYZUS"
+for i in alphabets:
+    print(i)
