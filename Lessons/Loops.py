6d7380bc512d49663c363865056789f7661f9442
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2023-07-16 09:24:40 +0530

    morning

---
+++ Lessons/Loops.py
@@ -0,0 +1,22 @@
+# For Loops ==>
+name='Shubham'
+for car in name :
+ print(car)
+ if (car=="h"):
+     print("This is a special!")
+     
+for k in range(8):
+    print (k)
+    
+for S in range(3, 6):
+    print(S)
+    
+for H in range(5,8):
+    print (H+1)
+    
+    
+# While Loops ==>
+num=15
+while(num>0):
+    print(num)
+    num=num-1
